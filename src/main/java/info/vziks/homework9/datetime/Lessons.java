3fafbeb253050c763bbc8f943713b7fe10f4bc02
Anton Prokhorov <vziks@live.ru>
2019-11-04 19:55:47 +0300

    (update) homework9+

--- src/main/java/info/vziks/homework9/datetime/Lessons.java
+++ src/main/java/info/vziks/homework9/datetime/Lessons.java
@@ -13,30 +13,30 @@ public class Lessons {
         Integer[] days = new Integer[]{Calendar.MONDAY, Calendar.WEDNESDAY, Calendar.FRIDAY};
 
         List<Integer> list = Arrays.asList(days);
-        Calendar startCal = Calendar.getInstance();
-        startCal.setTime(startDate);
+        Calendar startCalendar = Calendar.getInstance();
+        startCalendar.setTime(startDate);
 
-        Calendar endCal = Calendar.getInstance();
-        endCal.setTime(stopDate);
+        Calendar endCalendar = Calendar.getInstance();
+        endCalendar.setTime(stopDate);
 
         int lessons = 0;
 
-        if (startCal.getTimeInMillis() == endCal.getTimeInMillis()) {
+        if (startCalendar.getTimeInMillis() == endCalendar.getTimeInMillis()) {
             return 0;
         }
 
-        if (startCal.getTimeInMillis() > endCal.getTimeInMillis()) {
-            startCal.setTime(stopDate);
-            endCal.setTime(startDate);
+        if (startCalendar.getTimeInMillis() > endCalendar.getTimeInMillis()) {
+            startCalendar.setTime(stopDate);
+            endCalendar.setTime(startDate);
         }
 
         do {
 
-            startCal.add(Calendar.DAY_OF_MONTH, 1);
+            startCalendar.add(Calendar.DAY_OF_MONTH, 1);
 
-            if (list.contains(startCal.get(Calendar.DAY_OF_WEEK))) {
+            if (list.contains(startCalendar.get(Calendar.DAY_OF_WEEK))) {
 
-//                Date date = startCal.getTime();
+//                Date date = startCalendar.getTime();
 //
 //                String day = new SimpleDateFormat("dd/MM/yyyy").format(date);
 //
@@ -44,7 +44,7 @@ public class Lessons {
 
                 ++lessons;
             }
-        } while (startCal.getTimeInMillis() < endCal.getTimeInMillis()); //excluding end date
+        } while (startCalendar.getTimeInMillis() < endCalendar.getTimeInMillis()); //excluding end date
         return lessons;
     }
 }
