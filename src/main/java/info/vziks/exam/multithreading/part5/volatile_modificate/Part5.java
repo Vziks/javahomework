cb331194690b81bb561ea334b4413517e219800e
Anton Prokhorov <vziks@live.ru>
2019-12-06 17:24:01 +0300

    (update) multithreading part 5 volatile+

--- src/main/java/info/vziks/exam/multithreading/part5/volatile_modificate/Part5.java
+++ src/main/java/info/vziks/exam/multithreading/part5/volatile_modificate/Part5.java
@@ -1,10 +1,12 @@
 package info.vziks.exam.multithreading.part5.volatile_modificate;
 
+
 import java.util.Arrays;
 
 public class Part5 {
 
     private static volatile int value = 0;
+    private static volatile int i = 0;
 
     public static void main(String[] args) {
         MyThreadRead myThreadRead = new MyThreadRead();
@@ -16,14 +18,25 @@ public class Part5 {
     static class MyThreadWrite extends Thread {
         @Override
         public void run() {
+
             while (value < 5) {
                 ++value;
-                System.out.println("MyThreadRead value " + (value));
+                System.out.println("MyThreadWrite value " + (value));
                 try {
                     Thread.sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+            }
+
+            for (int j = 0; j < 100; j++) {
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("MyThreadWrite i" + j);
+                i = j;
             }
         }
     }
@@ -34,8 +47,17 @@ public class Part5 {
             int localVar = value;
             while (localVar < 5) {
                 if (localVar != value) {
-                    System.out.println("MyThreadWrite value " + value);
+                    System.out.println("MyThreadRead value " + value);
                     localVar = value;
+                }
+            }
+
+            for (int j = 0; j <  100; j++) {
+                System.out.println("MyThreadRead i" + i);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
 
