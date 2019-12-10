b66beb7df15aebfa65e3ed21e17e7f416a622cd7
Anton Prokhorov <vziks@live.ru>
2019-12-10 15:42:52 +0300

    (update) homework16 multithreading refactor

--- src/main/java/info/vziks/homework16/war/MultiParser.java
+++ src/main/java/info/vziks/homework16/war/MultiParser.java
@@ -6,68 +6,68 @@ import java.nio.file.Files;
 import java.util.*;
 
 public class MultiParser {
-    private ClassLoader loader;
+
+    private ClassLoader loader = MultiParser.class.getClassLoader();
     private File file;
-    private List<String> lines;
+    // Repository of all words from a file
     private List<String> fullStringArray = new ArrayList<>();
-    private Map<Integer, ProportionThread> integerProportionThreadMap = new HashMap<>();
+    // Array of ProportionThread instance
+    private List<ProportionThread> proportionThreadArrayList = new ArrayList<>();
+    // Summary HashMap <Word, Count>
     private Map<String, Integer> stringIntegerHashMap = new HashMap<>();
-
+    // Number of records seized
     private int proportion;
-    private int availableProcessors;
+    // Number of threads, default available processors
+    private int numberOfThreads = Runtime.getRuntime().availableProcessors();
 
-    public MultiParser(String fileName) throws IOException {
-        setLoader(MultiParser.class.getClassLoader());
+    public MultiParser(String fileName) {
         file = new File(Objects.requireNonNull(loader.getResource(fileName)).getFile());
-        lines = Files.readAllLines(getFile().toPath());
-        setAvailableProcessors(Runtime.getRuntime().availableProcessors());
     }
 
-    public void init() throws InterruptedException {
+    private void init() throws InterruptedException, IOException {
         fillStringArray();
         setProportion();
         distributeThread();
         combine();
     }
 
-    public void start() throws InterruptedException {
+    public void start() throws InterruptedException, IOException {
         init();
     }
 
     private void combine() {
-        for (Map.Entry<Integer, ProportionThread> entityProportion : integerProportionThreadMap.entrySet()) {
-            for (Map.Entry<String, Integer> entity : entityProportion.getValue().getStringIntegerHashMap().entrySet()) {
+        for (ProportionThread item :
+                proportionThreadArrayList) {
+            for (Map.Entry<String, Integer> entity : item.getStringIntegerHashMap().entrySet()) {
                 stringIntegerHashMap.merge(entity.getKey(), entity.getValue(), Integer::sum);
             }
         }
     }
 
     private void setProportion() {
-        this.proportion = fullStringArray.size() / availableProcessors;
+        this.proportion = fullStringArray.size() / numberOfThreads;
     }
 
-
-    public MultiParser setAvailableProcessors(int availableProcessors) {
-        this.availableProcessors = availableProcessors;
+    public MultiParser setNumberOfThreads(int numberOfThreads) {
+        this.numberOfThreads = numberOfThreads;
         return this;
     }
 
     private void distributeThread() throws InterruptedException {
-        for (int i = 0; i < this.availableProcessors; i++) {
-            integerProportionThreadMap.put(i, new ProportionThread(
+
+        for (int i = 0, check = 1; i < this.numberOfThreads; i++, check++) {
+            proportionThreadArrayList.add(new ProportionThread(
                     fullStringArray.subList(i * proportion,
-                            this.availableProcessors == i + 1 ? fullStringArray.size() : ((i + 1) * proportion) - 1)));
+                            this.numberOfThreads == check ? fullStringArray.size() : check * proportion - 1)));
         }
     }
 
-    private void fillStringArray() {
-        for (String line : lines) {
+    private void fillStringArray() throws IOException {
+        for (String line : Files.readAllLines(file.toPath())) {
             if (!"".equals(line)) {
                 String[] wordsSplit = line
                         .toLowerCase()
                         .replaceAll("\\p{Punct}", "")
-                        .replaceAll("\n", " ")
-                        .replaceAll("\r", " ")
                         .trim()
                         .split("\\s");
                 try {
@@ -79,23 +79,9 @@ public class MultiParser {
         }
     }
 
-    private MultiParser setLoader(ClassLoader loader) {
-        this.loader = loader;
-        return this;
-    }
-
-    private File getFile() {
-        return file;
-    }
-
-    private Map<String, Integer> getStringIntegerHashMap() {
-        return stringIntegerHashMap;
-    }
-
     public void printTop100Words() {
-
         System.out.println("Top 100 words");
-        getStringIntegerHashMap()
+        stringIntegerHashMap
                 .entrySet()
                 .stream()
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
